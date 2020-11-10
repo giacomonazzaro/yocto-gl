2a882b14f6803fea24e3d90e0817f4e09d03b472
Giacomo Nazzaro <nazzarogiacomo@gmail.com>
2020-11-10 18:08:33 +0100

    start

---
+++ libs/yocto_gui/yocto_window.cpp
@@ -0,0 +1,355 @@
+//
+// Utilities to use OpenGL 3, GLFW and ImGui.
+//
+
+//
+// LICENSE:
+//
+// Copyright (c) 2016 -- 2020 Fabio Pellacini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+// TODO(giacomo): review inlcudes
+#include "yocto_window.h"
+
+#include <yocto/yocto_commonio.h>
+
+#include <algorithm>
+#include <array>
+#include <cstdarg>
+#include <memory>
+#include <mutex>
+#include <stdexcept>
+#include <unordered_map>
+#include <utility>
+
+#include "ext/glad/glad.h"
+
+#ifdef __APPLE__
+#define GL_SILENCE_DEPRECATION
+#endif
+#include <GLFW/glfw3.h>
+
+#ifdef _WIN32
+#undef near
+#undef far
+#endif
+
+// -----------------------------------------------------------------------------
+// USING DIRECTIVES
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// using directives
+using std::array;
+using std::mutex;
+using std::pair;
+using std::unordered_map;
+using namespace std::string_literals;
+
+}  // namespace yocto
+
+// -----------------------------------------------------------------------------
+// UI APPLICATION
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// run the user interface with the give callbacks
+void run_ui(const vec2i& size, const string& title,
+    const gui_callbacks& callbacks, int widgets_width, bool widgets_left) {
+  auto win_guard = std::make_unique<gui_window>();
+  auto win       = win_guard.get();
+  init_window(win, size, title, (bool)callbacks.widgets_cb, widgets_width,
+      widgets_left);
+
+  set_init_callback(win, callbacks.init_cb);
+  set_clear_callback(win, callbacks.clear_cb);
+  set_draw_callback(win, callbacks.draw_cb);
+  set_widgets_callback(win, callbacks.widgets_cb);
+  set_drop_callback(win, callbacks.drop_cb);
+  set_key_callback(win, callbacks.key_cb);
+  set_char_callback(win, callbacks.char_cb);
+  set_click_callback(win, callbacks.click_cb);
+  set_scroll_callback(win, callbacks.scroll_cb);
+  set_update_callback(win, callbacks.update_cb);
+  set_uiupdate_callback(win, callbacks.uiupdate_cb);
+
+  // run ui
+  run_ui(win);
+
+  // clear
+  clear_window(win);
+}
+
+}  // namespace yocto
+
+// -----------------------------------------------------------------------------
+// UI WINDOW
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+static void draw_window(gui_window* win) {
+  glClearColor(win->background.x, win->background.y, win->background.z,
+      win->background.w);
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  if (win->draw_cb) win->draw_cb(win, win->input);
+  // if (win->widgets_cb) {
+  //   ImGui_ImplOpenGL3_NewFrame();
+  //   ImGui_ImplGlfw_NewFrame();
+  //   ImGui::NewFrame();
+  //   auto window = zero2i;
+  //   glfwGetWindowSize(win->win, &window.x, &window.y);
+  //   if (win->widgets_left) {
+  //     ImGui::SetNextWindowPos({0, 0});
+  //     ImGui::SetNextWindowSize({(float)win->widgets_width, (float)window.y});
+  //   } else {
+  //     ImGui::SetNextWindowPos({(float)(window.x - win->widgets_width), 0});
+  //     ImGui::SetNextWindowSize({(float)win->widgets_width, (float)window.y});
+  //   }
+  //   ImGui::SetNextWindowCollapsed(false);
+  //   ImGui::SetNextWindowBgAlpha(1);
+  //   if (ImGui::Begin(win->title.c_str(), nullptr,
+  //           ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoResize |
+  //               ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoCollapse |
+  //               ImGuiWindowFlags_NoSavedSettings)) {
+  //     win->widgets_cb(win, win->input);
+  //   }
+  //   ImGui::End();
+  //   ImGui::Render();
+  //   ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+  // }
+  glfwSwapBuffers(win->win);
+}
+
+void init_window(gui_window* win, const vec2i& size, const string& title,
+    bool widgets, int widgets_width, bool widgets_left) {
+  // init glfw
+  if (!glfwInit())
+    throw std::runtime_error("cannot initialize windowing system");
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if __APPLE__
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+  // create window
+  win->title = title;
+  win->win = glfwCreateWindow(size.x, size.y, title.c_str(), nullptr, nullptr);
+  if (win->win == nullptr)
+    throw std::runtime_error{"cannot initialize windowing system"};
+  glfwMakeContextCurrent(win->win);
+  glfwSwapInterval(1);  // Enable vsync
+
+  // set user data
+  glfwSetWindowUserPointer(win->win, win);
+
+  // set callbacks
+  glfwSetWindowRefreshCallback(win->win, [](GLFWwindow* glfw) {
+    auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+    draw_window(win);
+  });
+  glfwSetDropCallback(
+      win->win, [](GLFWwindow* glfw, int num, const char** paths) {
+        auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+        if (win->drop_cb) {
+          auto pathv = vector<string>();
+          for (auto i = 0; i < num; i++) pathv.push_back(paths[i]);
+          win->drop_cb(win, pathv, win->input);
+        }
+      });
+  glfwSetKeyCallback(win->win,
+      [](GLFWwindow* glfw, int key, int scancode, int action, int mods) {
+        auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+        if (win->key_cb) win->key_cb(win, key, (bool)action, win->input);
+      });
+  glfwSetCharCallback(win->win, [](GLFWwindow* glfw, unsigned int key) {
+    auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+    if (win->char_cb) win->char_cb(win, key, win->input);
+  });
+  glfwSetMouseButtonCallback(
+      win->win, [](GLFWwindow* glfw, int button, int action, int mods) {
+        auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+        if (win->click_cb)
+          win->click_cb(
+              win, button == GLFW_MOUSE_BUTTON_LEFT, (bool)action, win->input);
+      });
+  glfwSetScrollCallback(
+      win->win, [](GLFWwindow* glfw, double xoffset, double yoffset) {
+        auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+        if (win->scroll_cb) win->scroll_cb(win, (float)yoffset, win->input);
+      });
+  glfwSetWindowSizeCallback(
+      win->win, [](GLFWwindow* glfw, int width, int height) {
+        auto win = (gui_window*)glfwGetWindowUserPointer(glfw);
+        glfwGetWindowSize(
+            win->win, &win->input.window_size.x, &win->input.window_size.y);
+        if (win->widgets_width) win->input.window_size.x -= win->widgets_width;
+        glfwGetFramebufferSize(win->win, &win->input.framebuffer_viewport.z,
+            &win->input.framebuffer_viewport.w);
+        win->input.framebuffer_viewport.x = 0;
+        win->input.framebuffer_viewport.y = 0;
+        if (win->widgets_width) {
+          auto win_size = zero2i;
+          glfwGetWindowSize(win->win, &win_size.x, &win_size.y);
+          auto offset = (int)(win->widgets_width *
+                              (float)win->input.framebuffer_viewport.z /
+                              win_size.x);
+          win->input.framebuffer_viewport.z -= offset;
+          if (win->widgets_left) win->input.framebuffer_viewport.x += offset;
+        }
+      });
+
+  // init gl extensions
+  if (!gladLoadGL())
+    throw std::runtime_error{"cannot initialize OpenGL extensions"};
+
+  // widgets
+  //   if (widgets) {
+  //     ImGui::CreateContext();
+  //     ImGui::GetIO().IniFilename       = nullptr;
+  //     ImGui::GetStyle().WindowRounding = 0;
+  //     ImGui_ImplGlfw_InitForOpenGL(win->win, true);
+  // #ifndef __APPLE__
+  //     ImGui_ImplOpenGL3_Init();
+  // #else
+  //     ImGui_ImplOpenGL3_Init("#version 330");
+  // #endif
+  //     ImGui::StyleColorsDark();
+  //     win->widgets_width = widgets_width;
+  //     win->widgets_left  = widgets_left;
+  //   }
+}
+
+void clear_window(gui_window* win) {
+  glfwDestroyWindow(win->win);
+  glfwTerminate();
+  win->win = nullptr;
+}
+
+// Run loop
+void run_ui(gui_window* win) {
+  // init
+  if (win->init_cb) win->init_cb(win, win->input);
+
+  // loop
+  while (!glfwWindowShouldClose(win->win)) {
+    // update input
+    win->input.mouse_last = win->input.mouse_pos;
+    auto mouse_posx = 0.0, mouse_posy = 0.0;
+    glfwGetCursorPos(win->win, &mouse_posx, &mouse_posy);
+    win->input.mouse_pos = vec2f{(float)mouse_posx, (float)mouse_posy};
+    if (win->widgets_width && win->widgets_left)
+      win->input.mouse_pos.x -= win->widgets_width;
+    win->input.mouse_left = glfwGetMouseButton(
+                                win->win, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS;
+    win->input.mouse_right =
+        glfwGetMouseButton(win->win, GLFW_MOUSE_BUTTON_RIGHT) == GLFW_PRESS;
+    win->input.modifier_alt =
+        glfwGetKey(win->win, GLFW_KEY_LEFT_ALT) == GLFW_PRESS ||
+        glfwGetKey(win->win, GLFW_KEY_RIGHT_ALT) == GLFW_PRESS;
+    win->input.modifier_shift =
+        glfwGetKey(win->win, GLFW_KEY_LEFT_SHIFT) == GLFW_PRESS ||
+        glfwGetKey(win->win, GLFW_KEY_RIGHT_SHIFT) == GLFW_PRESS;
+    win->input.modifier_ctrl =
+        glfwGetKey(win->win, GLFW_KEY_LEFT_CONTROL) == GLFW_PRESS ||
+        glfwGetKey(win->win, GLFW_KEY_RIGHT_CONTROL) == GLFW_PRESS;
+    glfwGetWindowSize(
+        win->win, &win->input.window_size.x, &win->input.window_size.y);
+    if (win->widgets_width) win->input.window_size.x -= win->widgets_width;
+    glfwGetFramebufferSize(win->win, &win->input.framebuffer_viewport.z,
+        &win->input.framebuffer_viewport.w);
+    win->input.framebuffer_viewport.x = 0;
+    win->input.framebuffer_viewport.y = 0;
+    if (win->widgets_width) {
+      auto win_size = zero2i;
+      glfwGetWindowSize(win->win, &win_size.x, &win_size.y);
+      auto offset = (int)(win->widgets_width *
+                          (float)win->input.framebuffer_viewport.z /
+                          win_size.x);
+      win->input.framebuffer_viewport.z -= offset;
+      if (win->widgets_left) win->input.framebuffer_viewport.x += offset;
+    }
+    if (win->widgets_width) {
+      // TODO(giacomo): restore this
+      // auto io                   = &ImGui::GetIO();
+      // win->input.widgets_active = io->WantTextInput || io->WantCaptureMouse
+      // ||
+      //                             io->WantCaptureKeyboard;
+    }
+
+    // time
+    win->input.clock_last = win->input.clock_now;
+    win->input.clock_now =
+        std::chrono::high_resolution_clock::now().time_since_epoch().count();
+    win->input.time_now = (double)win->input.clock_now / 1000000000.0;
+    win->input.time_delta =
+        (double)(win->input.clock_now - win->input.clock_last) / 1000000000.0;
+
+    // update ui
+    if (win->uiupdate_cb && !win->input.widgets_active)
+      win->uiupdate_cb(win, win->input);
+
+    // update
+    if (win->update_cb) win->update_cb(win, win->input);
+
+    // draw
+    draw_window(win);
+
+    // event hadling
+    glfwPollEvents();
+  }
+
+  // clear
+  if (win->clear_cb) win->clear_cb(win, win->input);
+}
+
+void set_init_callback(gui_window* win, init_callback cb) { win->init_cb = cb; }
+void set_clear_callback(gui_window* win, clear_callback cb) {
+  win->clear_cb = cb;
+}
+void set_draw_callback(gui_window* win, draw_callback cb) { win->draw_cb = cb; }
+void set_widgets_callback(gui_window* win, widgets_callback cb) {
+  win->widgets_cb = cb;
+}
+void set_drop_callback(gui_window* win, drop_callback drop_cb) {
+  win->drop_cb = drop_cb;
+}
+void set_key_callback(gui_window* win, key_callback cb) { win->key_cb = cb; }
+void set_char_callback(gui_window* win, char_callback cb) { win->char_cb = cb; }
+void set_click_callback(gui_window* win, click_callback cb) {
+  win->click_cb = cb;
+}
+void set_scroll_callback(gui_window* win, scroll_callback cb) {
+  win->scroll_cb = cb;
+}
+void set_uiupdate_callback(gui_window* win, uiupdate_callback cb) {
+  win->uiupdate_cb = cb;
+}
+void set_update_callback(gui_window* win, update_callback cb) {
+  win->update_cb = cb;
+}
+
+void set_close(gui_window* win, bool close) {
+  glfwSetWindowShouldClose(win->win, close ? GLFW_TRUE : GLFW_FALSE);
+}
+
+}  // namespace yocto
---
+++ libs/yocto_gui/yocto_window.h
@@ -0,0 +1,185 @@
+//
+// Yocto/ImGui: Utilities for writing native GUIs with Dear ImGui and GLFW.
+//
+
+//
+// LICENSE:
+//
+// Copyright (c) 2016 -- 2020 Fabio Pellacini
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+//
+
+#ifndef _YOCTO_WINDOW_
+#define _YOCTO_WINDOW_
+
+#include <yocto/yocto_math.h>
+
+#include <functional>
+#include <string>
+#include <vector>
+
+// -----------------------------------------------------------------------------
+// USING DIRECTIVES
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// using directives
+using std::function;
+using std::string;
+using std::vector;
+
+}  // namespace yocto
+
+// forward declaration
+struct GLFWwindow;
+
+// -----------------------------------------------------------------------------
+// UI APPLICATION
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// Forward declaration of OpenGL window
+struct gui_window;
+
+// Input state
+struct gui_input {
+  bool     mouse_left           = false;  // left button
+  bool     mouse_right          = false;  // right button
+  bool     mouse_middle         = false;  // middle button
+  vec2f    mouse_pos            = {};     // position excluding widgets
+  vec2f    mouse_last           = {};  // last mouse position excluding widgets
+  vec2f    mouse_delta          = {};  // last mouse delta excluding widgets
+  bool     modifier_alt         = false;         // alt modifier
+  bool     modifier_ctrl        = false;         // ctrl modifier
+  bool     modifier_shift       = false;         // shift modifier
+  bool     widgets_active       = false;         // widgets are active
+  uint64_t clock_now            = 0;             // clock now
+  uint64_t clock_last           = 0;             // clock last
+  double   time_now             = 0;             // time now
+  double   time_delta           = 0;             // time delta
+  vec2i    window_size          = {0, 0};        // window size
+  vec4i    framebuffer_viewport = {0, 0, 0, 0};  // framebuffer viewport
+};
+
+// Init callback called after the window has opened
+using init_callback = function<void(gui_window*, const gui_input& input)>;
+// Clear callback called after the window is cloased
+using clear_callback = function<void(gui_window*, const gui_input& input)>;
+// Draw callback called every frame and when resizing
+using draw_callback = function<void(gui_window*, const gui_input& input)>;
+// Draw callback for drawing widgets
+using widgets_callback = function<void(gui_window*, const gui_input& input)>;
+// Drop callback that returns that list of dropped strings.
+using drop_callback =
+    function<void(gui_window*, const vector<string>&, const gui_input& input)>;
+// Key callback that returns key codes, pressed/released flag and modifier keys
+using key_callback =
+    function<void(gui_window*, int key, bool pressed, const gui_input& input)>;
+// Char callback that returns ASCII key
+using char_callback =
+    function<void(gui_window*, unsigned int key, const gui_input& input)>;
+// Mouse click callback that returns left/right button, pressed/released flag,
+// modifier keys
+using click_callback = function<void(
+    gui_window*, bool left, bool pressed, const gui_input& input)>;
+// Scroll callback that returns scroll amount
+using scroll_callback =
+    function<void(gui_window*, float amount, const gui_input& input)>;
+// Update functions called every frame
+using uiupdate_callback = function<void(gui_window*, const gui_input& input)>;
+// Update functions called every frame
+using update_callback = function<void(gui_window*, const gui_input& input)>;
+
+// User interface callcaks
+struct gui_callbacks {
+  init_callback     init_cb     = {};
+  clear_callback    clear_cb    = {};
+  draw_callback     draw_cb     = {};
+  widgets_callback  widgets_cb  = {};
+  drop_callback     drop_cb     = {};
+  key_callback      key_cb      = {};
+  char_callback     char_cb     = {};
+  click_callback    click_cb    = {};
+  scroll_callback   scroll_cb   = {};
+  update_callback   update_cb   = {};
+  uiupdate_callback uiupdate_cb = {};
+};
+
+// run the user interface with the give callbacks
+void run_ui(const vec2i& size, const string& title,
+    const gui_callbacks& callbaks, int widgets_width = 320,
+    bool widgets_left = true);
+
+}  // namespace yocto
+
+// -----------------------------------------------------------------------------
+// UI WINDOW
+// -----------------------------------------------------------------------------
+namespace yocto {
+
+// OpenGL window wrapper
+struct gui_window {
+  GLFWwindow*       win           = nullptr;
+  string            title         = "";
+  init_callback     init_cb       = {};
+  clear_callback    clear_cb      = {};
+  draw_callback     draw_cb       = {};
+  widgets_callback  widgets_cb    = {};
+  drop_callback     drop_cb       = {};
+  key_callback      key_cb        = {};
+  char_callback     char_cb       = {};
+  click_callback    click_cb      = {};
+  scroll_callback   scroll_cb     = {};
+  update_callback   update_cb     = {};
+  uiupdate_callback uiupdate_cb   = {};
+  int               widgets_width = 0;
+  bool              widgets_left  = true;
+  gui_input         input         = {};
+  vec4f             background    = {0.15f, 0.15f, 0.15f, 1.0f};
+};
+
+// Windows initialization
+void init_window(gui_window* win, const vec2i& size, const string& title,
+    bool widgets, int widgets_width = 320, bool widgets_left = true);
+
+// Window cleanup
+void clear_window(gui_window* win);
+
+// Set callbacks
+void set_init_callback(gui_window* win, init_callback init_cb);
+void set_clear_callback(gui_window* win, clear_callback clear_cb);
+void set_draw_callback(gui_window* win, draw_callback draw_cb);
+void set_widgets_callback(gui_window* win, widgets_callback widgets_cb);
+void set_drop_callback(gui_window* win, drop_callback drop_cb);
+void set_key_callback(gui_window* win, key_callback cb);
+void set_char_callback(gui_window* win, char_callback cb);
+void set_click_callback(gui_window* win, click_callback cb);
+void set_scroll_callback(gui_window* win, scroll_callback cb);
+void set_uiupdate_callback(gui_window* win, uiupdate_callback cb);
+void set_update_callback(gui_window* win, update_callback cb);
+
+// Run loop
+void run_ui(gui_window* win);
+void set_close(gui_window* win, bool close);
+
+}  // namespace yocto
+
+#endif
