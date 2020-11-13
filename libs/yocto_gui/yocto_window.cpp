62419fdb7276585cfae6c6b7579359b39da64874
Giacomo Nazzaro <nazzarogiacomo@gmail.com>
2020-11-13 10:37:19 +0100

    ysceneitraces

--- libs/yocto_gui/yocto_window.cpp
+++ libs/yocto_gui/yocto_window.cpp
@@ -58,6 +58,7 @@ namespace yocto {
 
 inline void update_button_from_input(gui_button& button, bool pressing) {
   if (pressing) {
+    // TODO(giacomo): solve this assert 
     assert(button.state != gui_button::state::down);
     button.state = gui_button::state::pressing;
   } else {
